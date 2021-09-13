a5a7e4bb13c4fa8382c31ad55f56f49db1b824de
geecrypt <gavina352@gmail.com>
2021-09-13 15:13:24 -0700

    implement ex 3

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,3 +1,7 @@
+# Exercise 5: Calculations
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
